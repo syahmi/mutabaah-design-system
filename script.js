5b809986f1529e192521945265e964b7684db104
Syahmi Ismail <msi@syamey.com>
2026-05-06 10:22:47 +0800

    1.7.5

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ themeToggleBtn.addEventListener('click', () => {
 });
 
 // ── Version ──
-const VERSION = '1.7.4';
+const VERSION = '1.7.5';
 const DATE = 'May 2026';
 
 function initVersionAndIcons() {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mutabaah-design-system-v1.7.4';
+const CACHE_NAME = 'mutabaah-design-system-v1.7.5';
 const ASSETS = [
   './',
   './index.html',
