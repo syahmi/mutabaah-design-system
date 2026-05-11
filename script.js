9357f3b341510dd80f84fa3c88d5a2cc19f20eb8
Syahmi Ismail <msi@syamey.com>
2026-05-11 22:14:34 +0800

    1.7.9

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ themeToggleBtn.addEventListener('click', () => {
 });
 
 // ── Version ──
-const VERSION = '1.7.8';
+const VERSION = '1.7.9';
 const DATE = 'May 2026';
 
 function initVersionAndIcons() {
